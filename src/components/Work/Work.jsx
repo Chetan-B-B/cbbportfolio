55e8ee4a504207b0123904422762d0df1e27cf81
Chetan-B-B <chetanchetu9740@gmail.com>
2025-01-21 14:23:44 +0530

    updated work page

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,21 +1,23 @@
+import { projects } from "../../constants";
+
 const Work = () => {
   return (
     <section id="work" className="min-h-screen px-6 py-12 ">
       <div className="mx-auto text-center max-w-7xl">
         {/* Work Grid */}
         <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
-          {[...Array(6)].map((_, index) => (
-            <div key={index} className="group">
+          {projects.map((project) => (
+            <div key={project.id} className="group">
               {/* Card */}
               <div className="relative  h-[28rem] rounded-2xl bg-white bg-opacity-45 border border-gray-200 shadow-lg overflow-hidden cursor-pointer">
                 <div className="relative h-full ">
                   <img
-                    src="https://framerusercontent.com/images/afCvIGY60qx8bPPHVXLeOJZE.jpg"
-                    alt={`Project ${index + 1}`}
+                    src={project.img1}
+                    alt={`${project.name}`}
                     className="absolute inset-0 object-cover w-full h-full"
                   />
                   <img
-                    src="https://framerusercontent.com/images/t1JiwWZvW2qWT31Dsu8iwpfSXSo.jpg"
+                    src= {project.img2}
                     alt="Overlay"
                     className="absolute inset-0 object-cover w-full h-full transition-opacity duration-300 opacity-0 group-hover:opacity-100"
                   />
@@ -24,12 +26,13 @@ const Work = () => {
               {/* Details Below Card */}
               <div className="flex justify-between mt-4">
                 <h3 className="text-lg font-bold text-gray-500 group-hover:text-gray-900">
-                  Project {index + 1}
+                  {project.name}
                 </h3>
 
                 <p className="space-x-4 text-sm text-gray-200 group-hover:text-black ">
-                  <span className="inline-block p-2 group-hover:bg-white bg-zinc-500 rounded-xl w-14 h-fit ">Web</span>
-                  <span className="inline-block p-2 rounded-xl w-14 h-fit bg-zinc-500 group-hover:bg-white">Cloud</span>
+                  {project.tags.map((tag, idx) => (
+                    <span key={idx} className="inline-block p-2 group-hover:bg-white bg-zinc-500 rounded-xl w-14 h-fit ">{ tag}</span>
+                  ))}
                 </p>
               </div>
             </div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -20,3 +20,34 @@ export const navLinks = [
     href: "#contact",
   },
 ];
+
+export const projects = [
+  {
+    id:1,
+    name: "Digital Enablement Platform",
+    img1: "/assets/work/cut-analytical-dashboard-1.png",
+    img2: "/assets/work/cut-survey-dashboard.png",
+    tags: ["Python","Flask","MonogDB"],
+  },
+  {
+    id:2,
+    name: "IaC Controller",
+    img1: "/assets/work/cut-analytical-dashboard-sideview (1).png",
+    img2: "/assets/work/cut-analytical-dashboard-full.png",
+    tags: [ "AWS", "Terraform", "Docker"],
+  },
+  {
+    id:3,
+    name: "Forkify APP",
+    img1: "/assets/work/forkify1.png",
+    img2: "/assets/work/forkify2.png",
+    tags: ["Javascript","MVC", "RESTAPI's"],
+  },
+  {
+    id:4,
+    name: "IaC Controller",
+    img1: "/assets/work/side-view3 (1).png",
+    img2: "/assets/work/migrate-to-as-code-login.svg",
+    tags: ["React","Tailwindcss",],
+  },
+]
