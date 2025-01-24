45c94b7ff33aef90d19d534a35f9619f203ab42c
Chetan-B-B <chetanchetu9740@gmail.com>
2025-01-24 19:45:15 +0530

    something is cooking

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -9,17 +9,17 @@ const Work = () => {
           {projects.map((project) => (
             <div key={project.id} className="group">
               {/* Card */}
-              <div className="relative  h-[28rem] rounded-2xl bg-white bg-opacity-45 border border-gray-200 shadow-lg overflow-hidden cursor-pointer">
-                <div className="relative h-full ">
+              <div className="relative  h-[28rem] rounded-2xl bg-white bg-opacity-45 border border-gray-200 shadow-lg overflow-hidden cursor-pointer [perspective:1000px]" >
+                <div className="relative h-full transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
                   <img
                     src={project.img1}
                     alt={`${project.name}`}
-                    className="absolute inset-0 object-contain object-top w-full h-full bg-white"
+                    className="absolute inset-0 object-contain object-top w-full h-full bg-white [backface-visibility:hidden]"
                   />
                   <img
                     src= {project.img2}
                     alt="Overlay"
-                    className="absolute inset-0 w-full h-full transition-opacity duration-300 opacity-0 group-hover:opacity-100"
+                    className="absolute inset-0 w-full h-full transition-opacity duration-500 opacity-0 group-hover:opacity-100  [transform:rotateY(180deg)] [backface-visibility:hidden]"
                   />
                 </div>
               </div>
